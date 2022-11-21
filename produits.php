941ad5276ad1f72d554208a2f860a27ba9c4c143
moahmed amine mezghich <git@mezghich.com>
2022-11-21 13:10:27 +0100

    méthode POST

--- produits.php
+++ produits.php
@@ -15,10 +15,49 @@ switch($request_method) {
             getProducts();
         }
         break;
+
+    case 'POST':
+			// Ajouter un produit
+			AddProduct();
+			break;
     default:
         // Invalid Request Method
         header("HTTP/1.0 405 Method Not Allowed");
         break;
+}
+
+function AddProduct()
+{
+    global $conn;
+    $data = json_decode(file_get_contents('php://input'), true);
+   
+    $name = $data["name"];
+    $description = $data["description"];
+    $price = $data["price"];
+    $category = $data["category"];
+    
+    $created = date('Y-m-d H:i:s');
+    $modified = date('Y-m-d H:i:s');
+
+
+    echo $query="INSERT INTO produit(name, description, price, category_id, created, modified) VALUES('".$name."', '".$description."', '".$price."', '".$category."', '".$created."', '".$modified."')";
+    
+    if(mysqli_query($conn, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'Produit ajouté avec succès.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'ERREUR!.'. mysqli_error($conn)
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
 
 function getProducts()
@@ -32,7 +71,7 @@ function getProducts()
 			$response[] = $row;
 		}
 		header('Content-Type: application/json');
-		echo json_encode($response, JSON_PRETTY_PRINT);
+		echo json_encode($response);
 	}
 
     function getProduct($id=0)
@@ -50,7 +89,7 @@ function getProducts()
 			$response[] = $row;
 		}
 		header('Content-Type: application/json');
-		echo json_encode($response, JSON_PRETTY_PRINT);
+		echo json_encode($response);
 	}
 
 ?>
